03a5beda55f2bf6c18bda9616853c3362602df64
Zestoneel <shreyashsde@gmail.com>
2024-01-31 12:15:15 +0530

    Created category-item component and added styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import './categories.styles.scss'
+import './components/category-item/category-item-component'
+import CategoryItem from './components/category-item/category-item-component';
 
 function App() {
 
@@ -32,17 +34,8 @@ function App() {
 
   return (
     <div className="categories-container">
-      {categories.map(({ id, title, imageUrl }) => (
-        <div key={id} className="category-container">
-          <div className="background-image" 
-          style={{
-            backgroundImage: `url(${imageUrl})`,
-          }}/>
-          <div className="category-body-container">
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category}/>
       ))}
     </div>
   );
