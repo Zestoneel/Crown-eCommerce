33f38388646a5b2c4a13b0af380166d69e949c23
Zestoneel <shreyashsde@gmail.com>
2024-02-05 19:04:57 +0530

    Created Routes, updated directory component and added navigation component

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,29 @@
+import { Outlet, Link } from "react-router-dom"
+import { Fragment } from "react";
+import './navigation.styles.scss'
+
+import CrwnLogo from '../../assets/crown.svg?react'
+
+const Navigation = () => {
+    return (
+        <Fragment>
+            <div className="navigation">
+                <Link className="logo-container" to='/'>
+                    {/* <img src='crown.svg' /> */}
+                    <CrwnLogo />
+                </Link>
+                <div className="nav-links-container">
+                    <Link className='nav-link' to='/shop'>
+                        SHOP
+                    </Link>
+                    <Link className='nav-link' to='/sign-in'>
+                        SIGN IN
+                    </Link>
+                </div>
+            </div>
+            <Outlet/>
+        </Fragment>
+    );
+};
+
+export default Navigation;
