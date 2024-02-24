bd3896fdd25248d03c05c2fd6ba0810735614eba
Zestoneel <shreyashsde@gmail.com>
2024-02-24 22:58:59 +0530

    Added navigation for directory.

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -18,9 +18,11 @@ const CartDropDown = () => {
     return (
         <div className='cart-dropdown-container'>
             <div className='cart-items'>
-                {cartItems.map((item) => (
+                {
+                    cartItems.length ? (cartItems.map((item) => (
                     <CartItem key={item.id} cartItem={item}/> 
-                ))}
+                ))) : ( <span>Your cart is empty.</span> )
+                }
             </div>
             <Button onClick={goToCheckoutHandler}>CHECKOUT</Button>
         </div>
--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,20 +1,25 @@
-/* eslint-disable react/prop-types */
+import { useNavigate } from 'react-router-dom';
+
 import './directory-item.styles.scss'
 
 const DirectoryItem = ({ category }) => {
-    const { imageUrl, title } = category;
-    return (
-        <div  className='directory-item-container'>
-          <div className='background-image' 
-          style={{
-            backgroundImage: `url(${imageUrl})`,
-          }}/>
-          <div className='body'>
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-    );
+  const { imageUrl, title, route } = category;
+  const navigate = useNavigate();
+
+  const onNavigatehandler = () => navigate(route);
+
+  return (
+    <div  className='directory-item-container' onClick={onNavigatehandler}>
+    <div className='background-image' 
+      style={{
+        backgroundImage: `url(${imageUrl})`,
+      }}/>
+      <div className='body'>
+        <h2>{title}</h2>
+        <p>Shop Now</p>
+      </div>
+    </div>
+  );
 };
 
 export default DirectoryItem;
