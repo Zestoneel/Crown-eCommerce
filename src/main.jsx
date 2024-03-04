021532e45a9b0d0860e73705eefd96884e15900e
Zestoneel <shreyashsde@gmail.com>
2024-03-04 22:57:11 +0530

    Migrating categories context into redux

--- src/main.jsx
+++ src/main.jsx
@@ -6,20 +6,17 @@ import { store } from './store/store.js';
 
 import App from './App.jsx';
 import './index.scss';
-import { CategoriesProvider } from './context/categories.context.jsx';
 import { CartProvider } from './context/cart.context.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        {/* <UserProvider> */}
-        <CategoriesProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </CategoriesProvider>
-        {/* </UserProvider> */}
+        {/* <CategoriesProvider> */}
+        <CartProvider>
+          <App />
+        </CartProvider>
+        {/* </CategoriesProvider> */}
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
