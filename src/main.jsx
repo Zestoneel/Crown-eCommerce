c0877696a910942e56abe94fd23c1946cf8acd99
Zestoneel <shreyashsde@gmail.com>
2024-03-16 18:55:31 +0530

    Migrating user reducer to redux toolkit

--- src/main.jsx
+++ src/main.jsx
@@ -12,11 +12,11 @@ import './index.scss';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </PersistGate>
+      {/* <PersistGate persistor={persistor}> */}
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+      {/* </PersistGate> */}
     </Provider>
   </React.StrictMode>
 );
